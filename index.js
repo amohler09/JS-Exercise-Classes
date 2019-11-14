3f7743244399261b33347f10846c47f2b2ae3aa9
Amber Mohler <ambermohler@imac.lan>
2019-11-14 15:10:38 -0500

    completed first challenge

--- index.js
+++ index.js
@@ -29,19 +29,35 @@ class Airplane {
 
 /*
   TASK 1
-    - Write a Person class whose constructor initializes `name` and `age` from arguments.
-    - All instances of Person should also initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
+    ** Write a Person class whose constructor initializes `name` and `age` from arguments.
+  ** All instances of Person should also initialize with an empty `stomach` array.
+    ** Give instances of Person the ability to `.eat("someFood")`:
+        ** When eating an edible, it should be pushed into the `stomach`.
+        ** The `eat` method should have no effect if there are 10 items in the `stomach`.
+    ** Give instances of Person the ability to `.poop()`:
+        ** When an instance poops, its `stomach` should empty.
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
 class Person {
-
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+    this.emptyStomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+    this.stomach.push(someFood); 
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
